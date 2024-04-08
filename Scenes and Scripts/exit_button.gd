7bbbb4a8d7b25ca900c51c390034e24ac0e1fde7
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-04-08 15:48:15 +0200

    game hub version 2
    sorry lars

--- Scenes and Scripts/exit_button.gd
+++ Scenes and Scripts/exit_button.gd
@@ -1,21 +1,13 @@
 extends Node2D
 
-
 var mouse_inside = false
 
 func _process(delta):
-	if mouse_inside:
-		$AnimatedSprite2D.play()
 	if mouse_inside and Input.is_action_just_pressed("left_click"):
-		get_tree().change_scene_to_file("res://Scenes and Scripts/test.tscn")
-	
+		get_tree().visible = false
 
 
 func _on_area_2d_mouse_entered():
 	mouse_inside = true
 func _on_area_2d_mouse_exited():
 	mouse_inside = false
-
-
-
-
--- Scenes and Scripts/job_notes.gd
+++ Scenes and Scripts/job_notes.gd
@@ -2,8 +2,11 @@ extends Node2D
 
 
 var mouse_inside = false
+var mouse_inside_job_note1 = false
 var outline_shader = preload("res://Shader/outline.tres")
 var no_shader = preload("res://Shader/no_shader.tres")
+var open_note_menu = false
+var open_note1 = 0
 
 
 func _process(delta):
@@ -11,9 +14,28 @@ func _process(delta):
 		$Sprite2D.material = outline_shader
 	else:
 		$Sprite2D.material = no_shader
+	if Input.is_action_just_pressed("left_click") and open_note_menu == true:
+		open_note_menu = false
+	if mouse_inside and Input.is_action_just_pressed("left_click"):
+		open_note_menu = true
+	if open_note_menu == true:
+		$JobNoteMenu.visible = true
+		$JobNoteMenu/InJobMenuJobNote1.visible = true
+	else:
+		$JobNoteMenu.visible = false
+		$JobNoteMenu/InJobMenuJobNote1.visible = false
 
+	if mouse_inside_job_note1 and Input.is_action_just_pressed("left_click"):
+		$JobNoteMenu/InJobMenuJobNote1/JobNote1.visible = true
 
+# for shaders
 func _on_area_2d_mouse_entered():
 	mouse_inside = true
 func _on_area_2d_mouse_exited():
 	mouse_inside = false
+
+
+func _on_area_2d_InJobMenuJobNote1_mouse_entered():
+	mouse_inside_job_note1 = true
+func _on_area_2d_InJobMenuJobNote1_mouse_exited():
+	mouse_inside_job_note1 = true
