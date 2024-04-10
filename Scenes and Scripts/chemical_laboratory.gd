33605ac9737a1a15f27604dfa5aec4b04e62e545
mrmetron <69351693+mrmetron@users.noreply.github.com>
2024-04-10 10:11:26 +0200

    labor exisitert

--- Scenes and Scripts/chemical_laboratory.gd
+++ Scenes and Scripts/chemical_laboratory.gd
@@ -1,15 +1,20 @@
 extends Node2D
+var mouse_on = false
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$TextureRect/DialogeText.text == "Ich brauche deine Hilfe! Ich kann michr nicht mehr sichtbar machen. Du musst mir meine Visabel-Mischung machen. Fülle zuerst Wasser in das erste Ragenzglas. "
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) && mouse_on == true:
+		$Laborraum.visible = false
 
 func _on_texture_button_button_down():
 	$TextureRect.visible = false
+
+
+func _on_labor_area_2d_mouse_entered():
+	mouse_on = true
+
+
+func _on_labor_area_2d_mouse_exited():
+	mouse_on = false
