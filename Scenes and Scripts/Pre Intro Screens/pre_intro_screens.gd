89a911413009852766ef5e68ab0c849fafa51fbb
Sw2ft <lars-kling@gmx.net>
2024-04-24 10:57:00 +0200

    josef wollte das so

--- Scenes and Scripts/Pre Intro Screens/pre_intro_screens.gd
+++ Scenes and Scripts/Pre Intro Screens/pre_intro_screens.gd
@@ -16,16 +16,16 @@ func _process(delta):
 func _on_animation_player_animation_finished(anim_name):
 	if animation_counter == 0:
 		$Fade/AnimationPlayer.play_backwards("fade_in")
-		await get_tree().create_timer(4).timeout
+		await get_tree().create_timer(2).timeout
 		$DDI.visible = true
 		animation_counter = 1
 	elif animation_counter == 1:
 		$Fade/AnimationPlayer.play("fade_in")
-		await get_tree().create_timer(4).timeout
+		await get_tree().create_timer(2).timeout
 		animation_counter = 2
 	elif animation_counter == 2:
 		$Fade/AnimationPlayer.play_backwards("fade_in")
-		await get_tree().create_timer(4).timeout
+		await get_tree().create_timer(2).timeout
 		animation_counter = 3
 	elif animation_counter == 3:
 		get_tree().change_scene_to_file("res://Scenes and Scripts/map.tscn")
