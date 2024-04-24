cdbe13bdadfcc850288127766900c804019ea1bb
Sw2ft <lars-kling@gmx.net>
2024-04-24 15:29:59 +0200

    tts weil yoshi cringe ist
    carl und cari voicelines sind drin + es clickt wenn man auswählt

--- Scenes and Scripts/Character Choice/character_choice.gd
+++ Scenes and Scripts/Character Choice/character_choice.gd
@@ -18,3 +18,32 @@ func _on_carl_button_button_up():
 func _on_cari_button_button_up():
 	Global.character = "Cari"
 	get_tree().change_scene_to_file("res://Scenes and Scripts/map.tscn")
+
+func _on_carl_button_button_down():
+	play_ui_sound()
+
+func _on_cari_button_button_down():
+	play_ui_sound()
+
+func _on_cari_button_mouse_entered():
+	if !$UI.playing:
+		$UI.stream = load("res://Assets/Sound Test/Voice Over/barks/vo_cari_intro01_var1.mp3")
+		$UI.play()
+
+func _on_carl_button_mouse_entered():
+	if !$UI.playing:
+		$UI.stream = load("res://Assets/Sound Test/Voice Over/barks/vo_carl_intro01_var1.mp3")
+		$UI.play()
+
+func play_ui_sound():
+	var random = randi() % 3
+	
+	match random:
+		0:
+			$UI.stream = load("res://Assets/Sound Test/sfx_hub_ui_click_var1.mp3")
+		1:
+			$UI.stream = load("res://Assets/Sound Test/sfx_hub_ui_click_var2.mp3")
+		2:
+			$UI.stream = load("res://Assets/Sound Test/sfx_hub_ui_click_var3.mp3")
+	
+	$UI.play()
