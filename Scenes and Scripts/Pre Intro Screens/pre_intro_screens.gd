78566592147105835d98776a37c3afc10e8e6cdc
Sw2ft <lars-kling@gmx.net>
2024-04-24 11:03:44 +0200

    Update pre_intro_screens.gd

--- Scenes and Scripts/Pre Intro Screens/pre_intro_screens.gd
+++ Scenes and Scripts/Pre Intro Screens/pre_intro_screens.gd
@@ -2,16 +2,6 @@ extends Control
 
 var animation_counter = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if animation_counter == 0:
@@ -28,4 +18,4 @@ func _on_animation_player_animation_finished(anim_name):
 		await get_tree().create_timer(2).timeout
 		animation_counter = 3
 	elif animation_counter == 3:
-		get_tree().change_scene_to_file("res://Scenes and Scripts/map.tscn")
+		get_tree().change_scene_to_file("res://Scenes and Scripts/Title Screen/title_screen.tscn")
