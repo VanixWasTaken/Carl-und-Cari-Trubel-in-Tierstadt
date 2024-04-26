b1333f98f02c310f90fed9ad7491bdd4749d465f
mrmetron <jasperfreh@gmail.com>
2024-04-26 18:13:58 +0200

    ok man kann laufen

--- Scenes and Scripts/3D/player_test.gd
+++ Scenes and Scripts/3D/player_test.gd
@@ -1,40 +1,34 @@
 extends CharacterBody3D
+@onready var navigation_agent:= $NavigationAgent3D
+var speed = 15
 
-# How fast the player moves in meters per second.
-@export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
-@export var fall_acceleration = 75
+func _process(delta):
+	if(navigation_agent.is_target_reached()):
+		return
+	move_to_target(delta)
 
-var target_velocity = Vector3.ZERO
-
-
-func _physics_process(delta):
-	# We create a local variable to store the input direction.
-	var direction = Vector3.ZERO
-
-	# We check for each move input and update the direction accordingly.
-	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		direction.z += 1
-	if Input.is_action_pressed("ui_up"):
-		direction.z -= 1
-		
-		
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-
-
-	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
-	# Moving the Character
-	velocity = target_velocity
+func move_to_target(delta):
+	var target_position = navigation_agent.target_position
+	var direction = global_position.direction_to(target_position)
+	
+	velocity = direction * speed
 	move_and_slide()
+
+func _input(event):
+# https://www.youtube.com/watch?v=KT06pv06Q1U Das ganze movement, versteh das alles nicht so 100 aber scheint erstmal zu klappen
+	if Input.is_action_just_pressed("left_click"):
+		var camera = get_tree().get_nodes_in_group("Camera")[0] #Holt die Kamera, da die Raycatsts von diueser aus entstehen die zur bewegung benötigt werden
+		var mouse_position = get_viewport().get_mouse_position() #als 2d value
+		var ray_length = 100 # Wie lang ist der raycast der geschossen wird?
+		var from = camera.project_ray_origin(mouse_position) # position von wo aus der raycast geschossen wird
+		var to = from + camera.project_ray_normal(mouse_position) * ray_length # endpunkt des raycatsts
+		var space = get_world_3d().direct_space_state # wollen wir in 2d oder 3d sein?
+		var ray_query = PhysicsRayQueryParameters3D.new() # beschrebt , was der raycast ist und was der macht
+		ray_query.from = from
+		ray_query.to = to
+		var result = space.intersect_ray(ray_query) #schießt den rayab und sag mir womit der intersected
+		print(result)
+		
+		navigation_agent.target_position = result.position
+
+
