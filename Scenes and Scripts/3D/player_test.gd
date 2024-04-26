d8431b3ea724c870f9c2b82fce50c01864bd6eed
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-04-26 16:29:51 +0200

    3d test 3

--- Scenes and Scripts/3D/player_test.gd
+++ Scenes and Scripts/3D/player_test.gd
@@ -8,9 +8,6 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 
 
-func _process(delta):
-	print(speed)
-
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
