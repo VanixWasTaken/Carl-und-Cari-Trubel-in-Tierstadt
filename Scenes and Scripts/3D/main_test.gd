455b7ce7368b4f4e5f602fc5a2366bdbc115b370
Sw2ft <lars-kling@gmx.net>
2024-04-29 11:41:04 +0200

    ich hoff das macht keine merge konflikte
    ganz viel audio stuff

--- Scenes and Scripts/3D/main_test.gd
+++ Scenes and Scripts/3D/main_test.gd
@@ -1,6 +1,14 @@
 extends Node3D
 
-
+func _ready():
+	var playback: AudioStreamPlaybackPolyphonic
+	$Ambience.play()
+	playback = $Ambience.get_stream_playback()
+	playback.play_stream(load("res://Assets/Sound Test/sfx_tutorial_ambience_birds_var1.mp3"))
+	playback.play_stream(load("res://Assets/Sound Test/sfx_tutorial_ambience_creaks_var1.mp3"))
+	playback.play_stream(load("res://Assets/Sound Test/sfx_tutorial_ambience_wind_var1.mp3"))
+	
+	MusicController._play_music("learning_by_doing", "tutorial")
 
 func _process(delta):
 	if $Player.position.x <= 19.5 and $Player.position.x >= -19.5:
