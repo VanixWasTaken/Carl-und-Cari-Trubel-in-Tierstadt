3272f2491b54840fb3659b6ddd669f001db4ad61
mrmetron <69351693+mrmetron@users.noreply.github.com>
2024-04-29 12:44:22 +0200

    ene mene muh und raus bist du

--- Scenes and Scripts/3D/main_test.gd
+++ Scenes and Scripts/3D/main_test.gd
@@ -3,6 +3,7 @@ extends Node3D
 func _ready():
 	var playback: AudioStreamPlaybackPolyphonic
 	$Ambience.play()
+	$Mouse.play("default")
 	playback = $Ambience.get_stream_playback()
 	playback.play_stream(load("res://Assets/Sound Test/sfx_tutorial_ambience_birds_var1.mp3"))
 	playback.play_stream(load("res://Assets/Sound Test/sfx_tutorial_ambience_creaks_var1.mp3"))
