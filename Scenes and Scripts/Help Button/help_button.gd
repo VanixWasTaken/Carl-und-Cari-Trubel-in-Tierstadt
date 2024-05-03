5efac56878f88e66fd37ffa41504a828ef922c5d
mrmetron <jasperfreh@gmail.com>
2024-05-03 17:59:57 +0200

    alles fertisch

--- Scenes and Scripts/Help Button/help_button.gd
+++ Scenes and Scripts/Help Button/help_button.gd
@@ -2,6 +2,8 @@ extends Control
 signal HelpOpened
 signal HelpClosed
 var helpbuttonopen = false
+var menu_open = false
+var mouse_inside = false
 @onready var outline_shader = preload("res://Shader/outline.tres")
 @onready var no_shader = preload("res://Shader/no_shader.tres")
 
@@ -17,19 +19,23 @@ func _ready():
 		$TextureButton.texture_pressed = load("res://Assets/Test/button_help.png.PNG")
 		$TextureButton.texture_hover = load("res://Assets/Test/button_help.png.PNG")
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if menu_open || mouse_inside:
+		Global.moving_allowed = false
+	elif !mouse_inside && !menu_open:
+		Global.moving_allowed = true
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 
 
 func _on_texture_button_button_up():
+	menu_open = true
 	if !helpbuttonopen and Global.character == "Cari":
 		emit_signal("HelpOpened")
 		$TextureButton.texture_normal = load("res://Assets/Test/button_help_gray.png")
 		$TextureButton.disabled = true
 		await get_tree().create_timer(7).timeout
 		emit_signal("HelpClosed")
+		menu_open = false
 		$TextureButton.disabled = false
 		$TextureButton.texture_normal = load("res://Assets/Test/button_help.png.PNG")
 	elif !helpbuttonopen and Global.character == "Carl":
@@ -38,15 +44,17 @@ func _on_texture_button_button_up():
 		$TextureButton.disabled = true
 		await get_tree().create_timer(7).timeout
 		emit_signal("HelpClosed")
+		menu_open = false
 		$TextureButton.disabled = false
 		$TextureButton.texture_normal = load("res://Assets/Test/button_help_cari.png")
 
 
 func _on_texture_button_mouse_entered():
 	$TextureButton.material = outline_shader
+	Global.moving_allowed = false
+	mouse_inside = true
 
 
 func _on_texture_button_mouse_exited():
 	$TextureButton.material = no_shader
-
-
+	mouse_inside = false
