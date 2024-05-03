b8c50a6644accb3bd8d8b7cc7be322b559ce5ccc
Sw2ft <147743626+Sw2ft@users.noreply.github.com>
2024-05-03 18:48:56 +0200

    glaspenis

--- Scenes and Scripts/Character Choice/character_choice.gd
+++ Scenes and Scripts/Character Choice/character_choice.gd
@@ -20,30 +20,15 @@ func _on_cari_button_button_up():
 	get_tree().change_scene_to_file("res://Scenes and Scripts/3D/main_test.tscn")
 
 func _on_carl_button_button_down():
-	play_ui_sound()
+	$UI.play()
 
 func _on_cari_button_button_down():
-	play_ui_sound()
+	$UI.play()
 
 func _on_cari_button_mouse_entered():
-	if !$UI.playing:
-		$UI.stream = load("res://Assets/Sound Test/Voice Over/barks/vo_cari_intro01_var1.mp3")
-		$UI.play()
+	$VO.stream = load("res://Assets/Sound Test/Voice Over/barks/Selection/vo_cari_intro01_var1.mp3")
+	$VO.play()
 
 func _on_carl_button_mouse_entered():
-	if !$UI.playing:
-		$UI.stream = load("res://Assets/Sound Test/Voice Over/barks/vo_carl_intro01_var1.mp3")
-		$UI.play()
-
-func play_ui_sound():
-	var random = randi() % 3
-	
-	match random:
-		0:
-			$UI.stream = load("res://Assets/Sound Test/sfx_hub_ui_click_var1.mp3")
-		1:
-			$UI.stream = load("res://Assets/Sound Test/sfx_hub_ui_click_var2.mp3")
-		2:
-			$UI.stream = load("res://Assets/Sound Test/sfx_hub_ui_click_var3.mp3")
-	
-	$UI.play()
+	$VO.stream = load("res://Assets/Sound Test/Voice Over/barks/Selection/vo_carl_intro01_var1.mp3")
+	$VO.play()
