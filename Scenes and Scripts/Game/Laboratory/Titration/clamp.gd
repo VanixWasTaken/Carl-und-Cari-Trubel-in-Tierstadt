0c874838e9ec84fff337653bd88be3351b926eaa
Sw2ft <lars-kling@gmx.net>
2024-05-08 15:52:10 +0200

    das macht alles kaputt

---
+++ Scenes and Scripts/Game/Laboratory/Titration/clamp.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+signal pouring_time
+
+@onready var outline_shader = preload("res://Shader/outline.tres")
+@onready var no_shader = preload("res://Shader/no_shader.tres")
+
+var holding = false
+var mouse_inside = false
+var in_stand = false
+var tube_counter = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if mouse_inside and Input.is_action_pressed("left_click"):
+		$ClampArea/Clamp.material = no_shader
+		var new_position = get_global_mouse_position()
+		global_position = new_position
+	elif Input.is_action_just_released("left_click"):
+		global_position = Vector2(938, 965)
+		if in_stand:
+			tube_counter += 1
+			emit_signal("pouring_time")
+			in_stand = false
+	
+	if tube_counter >> 3:
+		visible = true
+
+
+
+
+func _on_clamp_area_mouse_entered():
+	mouse_inside = true
+	$ClampArea/Clamp.material = outline_shader
+
+
+func _on_clamp_area_mouse_exited():
+	mouse_inside = false
+	$ClampArea/Clamp.material = no_shader
+
+
+func _on_clamp_area_area_entered(area):
+	if area.get_name() == "StandArea":
+		var children = area.get_children()
+		for child in children:
+			if child.name == "Stand" || child.name == "Glasses":
+				child.material = outline_shader
+		in_stand = true
+
+
+func _on_clamp_area_area_exited(area):
+	if area.get_name() == "StandArea":
+		var children = area.get_children()
+		for child in children:
+			if child.name == "Stand" || child.name == "Glasses":
+				child.material = no_shader
+		in_stand = false
---
+++ Scenes and Scripts/Game/Laboratory/Titration/titration.gd
@@ -0,0 +1,76 @@
+extends Node2D
+
+var ready_to_pour = false
+
+var tube_counter = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if ready_to_pour and Input.is_action_just_pressed("left_click"):
+		
+		if tube_counter <= 3:
+			if tube_counter == 1:
+				$PouringClamp/PouringClampAnim.play("pouring_green")
+			elif tube_counter == 2:
+				$PouringClamp/PouringClampAnim.play("pouring_purple")
+			elif tube_counter == 3:
+				$PouringClamp/PouringClampAnim.play("pouring_orange")
+			else:
+				$PouringClamp.visible = false
+		
+		
+		await get_tree().create_timer(3).timeout
+		
+		$PouringClamp.visible = false
+		
+		############################## HANDLES THE JUG AND STAND SPRITES ################################
+		match tube_counter:
+			1:
+				$Jug/Jug.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Jug/minigame2_glass_stage2.png")
+				$Stand/StandArea/Glasses.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Tubes/minigame2_testtubes_stage3.PNG")
+			2:
+				$Jug/Jug.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Jug/minigame2_glass_stage3.png")
+				$Stand/StandArea/Glasses.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Tubes/minigame2_testtubes_stage5.PNG")
+			3:
+				$Jug/Jug.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Jug/minigame2_glass_stage4.png")
+				$Stand/StandArea/Glasses.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Tubes/minigame2_testtubes_stage8.PNG")
+		
+		
+		$Clamp.visible = true
+		ready_to_pour = false
+
+
+# handles the colors and general visibility, whenever the clamp is dragged onto the stand, caps at tube_counter = 4 --> no overloading variable
+func _on_clamp_pouring_time():
+	
+	
+	if tube_counter <= 3:
+		
+		
+		
+		tube_counter += 1
+		$Clamp.visible = false
+		
+		if tube_counter == 1:
+			$PouringClamp.visible = true
+			$PouringClamp/PouringClampAnim.play("idle_green")
+			$Stand/StandArea/Glasses.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Tubes/minigame2_testtubes_stage2.PNG")
+			ready_to_pour = true
+		elif tube_counter == 2:
+			$PouringClamp.visible = true
+			$PouringClamp/PouringClampAnim.play("idle_purple")
+			$Stand/StandArea/Glasses.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Tubes/minigame2_testtubes_stage4.PNG")
+			ready_to_pour = true
+		elif tube_counter == 3:
+			$PouringClamp.visible = true
+			$PouringClamp/PouringClampAnim.play("idle_orange")
+			$Stand/StandArea/Glasses.texture = load("res://Assets/Art/Environment/Rooms/Laboratory/Minigames/Titration/Tubes/minigame2_testtubes_stage7.PNG")
+			ready_to_pour = true
+	else:
+		$Clamp.visible = true
+	
