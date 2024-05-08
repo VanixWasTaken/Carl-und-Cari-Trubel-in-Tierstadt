5ba796440f2cbdc75f30cbafd21ce6532bad66b9
mrmetron <69351693+mrmetron@users.noreply.github.com>
2024-05-08 11:18:34 +0200

    klein aber klein

---
+++ Scenes and Scripts/Game/Laboratory/laboratory_3d.gd
@@ -0,0 +1,19 @@
+extends Node3D
+
+func _ready():
+	var playback: AudioStreamPlaybackPolyphonic
+	$Ambience.play()
+	playback = $Ambience.get_stream_playback()
+	playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_birds_var1.mp3"))
+	playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_creaks_var1.mp3"))
+	playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_wind_var1.mp3"))
+	
+	MusicController._play_music("learning_by_doing", "tutorial")
+
+func _process(delta):
+	if $Player.position.x <= 19.5 and $Player.position.x >= -19.5:
+		$Camera3D.position.x = $Player.position.x
+
+func _on_door_area_3d_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene_to_file("res://Scenes and Scripts/Game/Map/map.tscn")
