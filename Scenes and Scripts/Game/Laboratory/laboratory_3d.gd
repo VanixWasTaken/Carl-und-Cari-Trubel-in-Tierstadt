3f1fa83c72aa4fbc7fa566a9cc9cca178cc7b514
Sw2ft <147743626+Sw2ft@users.noreply.github.com>
2024-05-09 12:50:20 +0200

    musik japseer jat babutt macht

--- Scenes and Scripts/Game/Laboratory/laboratory_3d.gd
+++ Scenes and Scripts/Game/Laboratory/laboratory_3d.gd
@@ -13,14 +13,14 @@ func _ready():
 	elif Global.lab_cutscene_played == true:
 		$Objects/Chameleon.queue_free()
 		$CameraPan.queue_free()
-	var playback: AudioStreamPlaybackPolyphonic
-	$Ambience.play()
-	playback = $Ambience.get_stream_playback()
-	playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_birds_var1.mp3"))
-	playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_creaks_var1.mp3"))
-	playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_wind_var1.mp3"))
+	#var playback: AudioStreamPlaybackPolyphonic
+	#$Ambience.play()
+	#playback = $Ambience.get_stream_playback()
+	#playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_birds_var1.mp3"))
+	#playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_creaks_var1.mp3"))
+	#playback.play_stream(load("res://Assets/Sound/SFX/Ambience/Tutorial/sfx_tutorial_ambience_wind_var1.mp3"))
 	
-	MusicController._play_music("learning_by_doing", "tutorial")
+	MusicController._play_music("titration_trap", "laboratory")
 
 func _process(delta):
 	if !cutscene:
