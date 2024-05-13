623d5aaab02c677303c00cc129ba58a3a2b38c52
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-05-13 14:09:49 +0200

    sink asset switch

--- Scenes and Scripts/Game/Laboratory/Objects/sink.gd
+++ Scenes and Scripts/Game/Laboratory/Objects/sink.gd
@@ -1,14 +1,14 @@
 extends Sprite3D
 
 var no_shader = preload("res://Assets/Art/Environment/Rooms/Laboratory/Objects/lab_cabinet_right.png")
-var outline_shader = preload("res://Assets/Art/Environment/Rooms/Laboratory/Objects/lab_cabinet_right.png")
+var outline_shader = preload("res://Assets/Art/Environment/Rooms/Laboratory/Objects/lab_cabinet_right_highlight.png")
 
 
 
 
-#func _on_area_3d_mouse_entered():
-	#$".".texture = outline_shader
+func _on_area_3d_mouse_entered():
+	$".".texture = outline_shader
 
-#func _on_area_3d_mouse_exited():
-	#$".".texture = no_shader
+func _on_area_3d_mouse_exited():
+	$".".texture = no_shader
 
