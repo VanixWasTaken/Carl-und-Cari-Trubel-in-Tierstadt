bb6b83342dcbed1d937efdc6f629ec6d9ffb6e2c
SlivzSimon <129986575+SlivzSimon@users.noreply.github.com>
2024-06-06 13:00:37 +0200

    quiz progress

--- Scenes and Scripts/Game/Quiz/quiz_laboratory.gd
+++ Scenes and Scripts/Game/Quiz/quiz_laboratory.gd
@@ -9,3 +9,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_weiter_button_button_down():
+	$"Frage 2".visible=true
+	$"Frage 1".visible=false
+#funktioniert noch nicht
