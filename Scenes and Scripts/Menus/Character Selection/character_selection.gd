eb1959c79a5f72993b581c64ebeaae6bf5a799a3
Sw2ft <lars-kling@gmx.net>
2024-06-13 12:11:30 +0200

    kleines sounding

--- Scenes and Scripts/Menus/Character Selection/character_selection.gd
+++ Scenes and Scripts/Menus/Character Selection/character_selection.gd
@@ -3,15 +3,17 @@ extends Node2D
 
 func _on_carl_button_button_up():
 	Global.character = "Carl"
+	$CanvasLayer/AnimationPlayer.play("fade_out")
 	MusicController._fade_to_volume(-80, 1)
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(tutorial_scene)
+	$"CariButton/Cari Button".disabled = true
+	$"CarlButton/Carl Button".disabled = true
 
 func _on_cari_button_button_up():
 	Global.character = "Cari"
+	$CanvasLayer/AnimationPlayer.play("fade_out")
 	MusicController._fade_to_volume(-80, 1)
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(tutorial_scene)
+	$"CarlButton/Carl Button".disabled = true
+	$"CariButton/Cari Button".disabled = true
 
 func _on_carl_button_button_down():
 	$UI.play()
@@ -20,9 +22,16 @@ func _on_cari_button_button_down():
 	$UI.play()
 
 func _on_cari_button_mouse_entered():
-	$VO.stream = load("res://Assets/Sound/VO/Character Selection/Barks/Cari/vo_cari_intro01_var1.mp3")
-	$VO.play()
+	if !$"CariButton/Cari Button".disabled:
+		$VO.stream = load("res://Assets/Sound/VO/Character Selection/Barks/Cari/vo_cari_intro01_var1.mp3")
+		$VO.play()
 
 func _on_carl_button_mouse_entered():
-	$VO.stream = load("res://Assets/Sound/VO/Character Selection/Barks/Carl/vo_carl_intro01_var1.mp3")
-	$VO.play()
+	if !$"CarlButton/Carl Button".disabled:
+		$VO.stream = load("res://Assets/Sound/VO/Character Selection/Barks/Carl/vo_carl_intro01_var1.mp3")
+		$VO.play()
+
+
+
+func _on_animation_player_animation_finished(anim_name):
+	get_tree().change_scene_to_packed(tutorial_scene)
