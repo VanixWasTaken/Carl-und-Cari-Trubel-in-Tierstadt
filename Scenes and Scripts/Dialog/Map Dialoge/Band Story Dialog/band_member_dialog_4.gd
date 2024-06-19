56dfb8dbb6a217e9221521eccf39f34fe6f5822d
mrmetron <69351693+mrmetron@users.noreply.github.com>
2024-06-19 12:57:43 +0200

    kleines dialog noch gemacht und das neue icon eingefügt

---
+++ Scenes and Scripts/Dialog/Map Dialoge/Band Story Dialog/band_member_dialog_4.gd
@@ -0,0 +1,194 @@
+extends Control
+
+
+@onready var l_bubble_node = preload("res://Scenes and Scripts/Dialog/Ground Structure/speech_bubble_left.tscn")
+@onready var r_bubble_node = preload("res://Scenes and Scripts/Dialog/Ground Structure/speech_bubble_right.tscn")
+
+var dialogs = 1
+var dialog_side = ["left", "right"]
+var finished = false
+var same_speaker = false
+var pc
+var PRESET1
+var PRESET2
+var PRESET3
+var PRESET4
+var mouse_inside_area = false
+var player 
+################################  PUT CHARACTER ICONS HERE  ###############################
+
+var Carl = preload("res://Assets/Art/Characters/Carl/Dialog Icon/carl_dialog.png")
+var Cari = preload("res://Assets/Art/Characters/Cari/Dialog Icon/cari_dialog.png")
+var Samuel = preload("res://Assets/Art/Characters/Band Members/Singer Samuel/samuel_dialog_icon.png")
+var Barbara = preload("res://Assets/Art/Characters/Band Members/Bassist Barbara/barbara_icon.png")
+##########################################################################################
+
+
+
+func _ready():
+	player = get_tree().get_first_node_in_group("Player")
+	Global.moving_allowed = false
+	Global.dialog_playing= true
+	pc = Global.character
+	PRESET1 = Carl
+	PRESET2 = Cari
+	PRESET3 = Samuel
+	PRESET4 = Barbara
+
+###############################  PUT THE STARTING SIDE HERE  #############################
+	add_left_dialog_box()
+##########################################################################################
+
+
+
+
+
+
+
+func _process(delta):
+################################  PUT DIALOG NUMBER HERE  ################################
+	if dialogs == 8:
+		Global.dialog_playing= false
+		queue_free()
+##########################################################################################
+
+
+
+
+
+
+
+func add_left_dialog_box():
+	var node = l_bubble_node.instantiate()
+	$".".add_child(node)
+	dialog_side = "left"
+##################################  WRITE DIALOG HERE  ###################################
+	
+	if dialogs == 1:
+		var short_node = get_child(1)
+		var short_node_text = short_node.get_child(0)
+		var short_node_rect = short_node.get_child(1)
+		var short_node_name = short_node.get_child(3).get_child(0)
+		short_node_text.text = "Scheint als hättet ihr euch vertragen."
+		short_node_rect.texture = PRESET2
+		short_node_name.text = "Cari"
+		dialogs += 1
+
+	elif dialogs == 4:
+		var short_node = get_child(2)
+		var short_node_text = short_node.get_child(0)
+		var short_node_rect = short_node.get_child(1)
+		var short_node_name = short_node.get_child(3).get_child(0)
+		short_node_text.text = "Yeah! Dann sind wir einen Schritt näher am besten Konzert aller Zeiten."
+		short_node_rect.texture = PRESET1
+		short_node_name.text = "Carl"
+		dialogs += 1
+		same_speaker = true
+
+
+	elif dialogs == 5:
+		var short_node = get_child(2)
+		var short_node_text = short_node.get_child(0)
+		var short_node_rect = short_node.get_child(1)
+		var short_node_name = short_node.get_child(3).get_child(0)
+		short_node_text.text = "Habt ihr eine Ahnung, wo das nächste Mitglied sein könnte?"
+		short_node_rect.texture = PRESET2
+		short_node_name.text = "Cari"
+		dialogs += 1
+		same_speaker = false
+
+	elif dialogs == 7:
+		dialogs += 1
+		Global.moving_allowed = true
+##########################################################################################
+
+
+
+
+
+
+
+
+func add_right_dialog_box():
+	var node = r_bubble_node.instantiate()
+	$".".add_child(node)
+	dialog_side = "right"
+##################################  WRITE DIALOG HERE  ###################################
+	
+	if dialogs == 2:
+		var short_node = get_child(2)
+		var short_node_text = short_node.get_child(0)
+		var short_node_rect = short_node.get_child(1)
+		var short_node_name = short_node.get_child(3).get_child(0)
+		short_node_text.text = "Ja, dank euch, es hat sich herausgestellt, dass es sich die ganze Zeit nur um ein Missverständnis gehandelt hat."
+		short_node_rect.texture = PRESET4
+		short_node_name.text = "Barbara"
+		dialogs += 1
+		same_speaker = true
+
+	elif dialogs == 3:
+		var short_node = get_child(2)
+		var short_node_text = short_node.get_child(0)
+		var short_node_rect = short_node.get_child(1)
+		var short_node_name = short_node.get_child(3).get_child(0)
+		short_node_text.text = "Da kann ich mich nur anschließen. Ohne euch hätte ich keine Hoffnung mehr auf das Konzert, aber jetzt sehe ich wieder das Konzert vor meinen Augen."
+		short_node_rect.texture = PRESET3
+		short_node_name.text = "Samuel"
+		dialogs += 1
+		same_speaker = false
+
+	elif dialogs == 6:
+		var short_node = get_child(2)
+		var short_node_text = short_node.get_child(0)
+		var short_node_rect = short_node.get_child(1)
+		var short_node_name = short_node.get_child(3).get_child(0)
+		short_node_text.text = " Im Moment nicht, wir hören uns weiterhin um. Bianca meinte, dass ihr in der Zwischenzeit schauen sollt ob noch andere Leute eure Hilfe brauchen."
+		short_node_rect.texture = PRESET3
+		short_node_name.text = "Samuel"
+		dialogs += 1
+
+
+##########################################################################################
+
+
+
+func _input(event):
+	if mouse_inside_area == true:
+		if Input.is_action_just_pressed("left_click") && dialogs == 4:
+			Global.dialog_playing = false
+			Global.moving_allowed = true
+			dialogs == 5
+			player.navigation_agent.target_position = Vector3(-21.27536, 2.513487, -0.298849) 
+			$"../Area2D".queue_free()
+			queue_free()
+
+
+
+
+
+
+func _on_skip_button_pressed():
+	var short_node = get_child(1)
+	$SkipButton.visible = false
+	short_node.queue_free()
+	if dialog_side == "left" and !finished and same_speaker:
+		add_left_dialog_box()
+	elif dialog_side == "left" and !finished:
+		add_right_dialog_box()
+	elif dialog_side == "right" and !finished and same_speaker:
+		add_right_dialog_box()
+	elif dialog_side == "right" and !finished:
+		add_left_dialog_box()
+	await get_tree().create_timer(1).timeout
+	$SkipButton.visible = true
+
+
+
+func _on_skip_button_timer_timeout():
+	$SkipButton.visible = true
+
+func _on_skip_button_mouse_entered():
+	$"../Hover".play()
+
+func _on_skip_button_button_down():
+	$"../Click".play()
