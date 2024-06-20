44943c7966f096e652489a026437336035351513
mrmetron <69351693+mrmetron@users.noreply.github.com>
2024-06-20 15:19:36 +0200

    kack scheiß wixx penis arsch doppeltes =

--- Scenes and Scripts/Dialog/Map Dialoge/Band Story Dialog/band_member_dialog_2.gd
+++ Scenes and Scripts/Dialog/Map Dialoge/Band Story Dialog/band_member_dialog_2.gd
@@ -45,6 +45,8 @@ func _ready():
 func _process(delta):
 ################################  PUT DIALOG NUMBER HERE  ################################
 	if dialogs == 14:
+		#if Global.completed_jobs[0] == "Laboratory Building":
+			#GlobalBand.needed_animation = "BarbaraMarket1"
 		get_tree().get_first_node_in_group("AnimationPlayer").play(GlobalBand.needed_animation)
 		get_tree().get_first_node_in_group("BarbaraSprite").play("barbara_walking")
 		queue_free()
--- Scenes and Scripts/Game/Map/NPCs/npc_handler.gd
+++ Scenes and Scripts/Game/Map/NPCs/npc_handler.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @export var needed_animation = ""
-
+var barbara
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	barbara = get_tree().get_first_node_in_group("Barbara")
 	if GlobalBand.got_barbara && !GlobalBand.completed_quest:
-		$NBarbaraNPC.queue_free()
+		$BarbaraNPC.queue_free()
 		$SamuelNPC.hide()
 		$BarbaraAndSamuelNPC.show()
 
@@ -17,19 +18,21 @@ func _process(delta):
 
 func _on_npc_movement_animation_finished(anim_name):
 	if anim_name.contains("BarbaraMarket"):
+		barbara.get_parent().remove_child(barbara)
+		add_child(barbara)
 		Global.dialog_playing = false
 		$BarbaraAndSamuelNPC.show()
 		$SamuelNPC.hide()
-		$BarbaraNPC.queue_free()
+		barbara.queue_free()
 		
 
 func show_barbara():
-	print("GUgel das mal")
-	$BarbaraNPC.show()
-	if Global.completed_jobs != null:
+	barbara.show()
+	if Global.completed_jobs.size() != 0:
 		if Global.completed_jobs[0] == "Laboratory Building":
-			global_position = $BarbaraMarkers/GardenMarker.global_position
-			GlobalBand.needed_animation == "BarbaraMarket1"
+			GlobalBand.needed_animation = "BarbaraMarket1"
+			barbara.global_position = $BarbaraMarkers/GardenMarker.global_position
 		else:
 			global_position = $BarbaraMarkers/LaboratoryMarker.global_position
 			GlobalBand.needed_animation = "BarbaraMarket2"
+			barbara.global_position = $BarbaraMarkers/LaboratoryMarker.global_position
