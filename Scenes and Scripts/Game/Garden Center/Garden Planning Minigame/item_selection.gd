58a037a6632e39dff6d2c0c2568626a5c1895ff2
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-06-27 17:00:45 +0200

    worked on minigame 3

--- Scenes and Scripts/Game/Garden Center/Garden Planning Minigame/item_selection.gd
+++ Scenes and Scripts/Game/Garden Center/Garden Planning Minigame/item_selection.gd
@@ -1,12 +1,20 @@
 extends Node
 
 @onready var arrows = $Arrows
-
+var current_item_selected: String = "PlantPot" # There is PlantPot, StonePlate, Flower
 
 
 func _on_arrows_move_left():
-	if arrows.item_dict == "PlantPot":
-		$AnimationPlayer.play("pick_stone")
+	if current_item_selected == "PlantPot":
+		$AnimationPlayer.play("plant_pot_to_stone_plate")
+		current_item_selected = "StonePlate"
+	
+
 
 func _on_arrows_move_right():
-	pass # Replace with function body.
+	if current_item_selected == "StonePlate":
+		$AnimationPlayer.play("stone_plate_to_plant_pot")
+		current_item_selected = "PlantPot"
+	elif current_item_selected == "PlantPot":
+		$AnimationPlayer.play("plant_pot_to_flower")
+		current_item_selected = "Flower"
