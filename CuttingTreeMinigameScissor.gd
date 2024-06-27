3e0610a24b0f02f3850292c0145a6fdb2d4776f1
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-06-27 12:03:39 +0200

    Added minigame 1

--- CuttingTreeMinigameScissor.gd
+++ CuttingTreeMinigameScissor.gd
@@ -1,14 +1,20 @@
 extends Sprite2D
 
-@onready var scissor = preload("res://Assets/Art/Environment/Rooms/Garden Center/CuttingTreeMinigame/scissors1.png")
+@onready var scissor_uncut = preload("res://Assets/Art/Environment/Rooms/Garden Center/CuttingTreeMinigame/scissors1.png")
 @onready var scissor_cut = preload("res://Assets/Art/Environment/Rooms/Garden Center/CuttingTreeMinigame/scissors2.png")
+
+@onready var scissor_spr = $"."
+
 
 
 func _input(event):
 	if event.is_action_pressed("left_click"):
-		scissor_cut
-	
+		scissor_spr.texture = scissor_cut
+
+	if event.is_action_released("left_click"):
+		scissor_spr.texture = scissor_uncut
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#position = get_global_mouse_position()
+func _process(delta):
+	position = get_global_mouse_position()
+
