b6df669f07646724ba99251a8b938a71f39f93bf
Sw2ft <lars-kling@gmx.net>
2024-06-27 14:49:56 +0200

    Update garden_center.gd

--- Scenes and Scripts/Game/Garden Center/garden_center.gd
+++ Scenes and Scripts/Game/Garden Center/garden_center.gd
@@ -3,6 +3,7 @@ extends Node3D
 var player
 
 func _ready():
+	MusicController._play_music("peace_and_tranquility", "garden_center", -15)
 	Global.last_scene = "GardenCenter"
 	player = get_tree().get_first_node_in_group("Player")
 	if GlobalGarden.cutscene_played == false:
