303434cff64df111ab2528dfcf2f7f5bf00a4935
mrmetron <69351693+mrmetron@users.noreply.github.com>
2024-06-27 11:35:23 +0200

    camera fahrt ist feritsch

--- Scenes and Scripts/Game/Garden Center/garden_center.gd
+++ Scenes and Scripts/Game/Garden Center/garden_center.gd
@@ -1,21 +1,22 @@
 extends Node3D
 
-var cutscene = true
+var player
 
 func _ready():
-	if Global.garden_cutscene_played == false:
-		cutscene = true
-		$CameraPan.play("camera_pan")
+	Global.last_scene = "GardenCenter"
+	player = get_tree().get_first_node_in_group("Player")
+	if GlobalGarden.cutscene_played == false:
+		$CutsceneAnimation.play("camera_pan")
+		Global.cutscene_playing = true
 	
 
 func _process(_delta):
-	if !cutscene:
+	if !Global.cutscene_playing:
 		if $Player.position.x <= 19.5 and $Player.position.x >= -19.5:
 			$Camera3D.position.x = $Player.position.x
 
 
 
 func _on_cutscene_animation_animation_finished(camera_pan):
-	cutscene = false
-	Global.garden_cutscene_played = true
+	Global.cutscene_playing = false
 	$CutsceneAnimation/MouseBlock.queue_free()
