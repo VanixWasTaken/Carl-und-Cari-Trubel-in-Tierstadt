07c0ede94556306b302a6092fe48f7237ca4d5c7
Sw2ft <147743626+Sw2ft@users.noreply.github.com>
2024-07-05 17:57:00 +0200

    dialog vier dialog schmier denk mal drüber nach

--- Scenes and Scripts/Dialog/Garden Dialog/garden_center_dialog_4.gd
+++ Scenes and Scripts/Dialog/Garden Dialog/garden_center_dialog_4.gd
@@ -9,6 +9,10 @@ var dialog_side = ["left", "right"]
 var finished = false
 var same_speaker = false
 
+var carl_path = "res://Assets/Sound/VO/Garden Center/Carl/PC/vo_pc_carl_garden_center_dialog_"
+var cari_path = "res://Assets/Sound/VO/Garden Center/Cari/PC/vo_pc_cari_garden_center_dialog_"
+var guido_path = "res://Assets/Sound/VO/Garden Center/Guido/vo_npc_guido_garden_center_dialog_"
+
 ################################  PUT CHARACTER ICONS HERE  ###############################
 
 var PRESET1 = preload("res://Assets/Art/Characters/Carl/Dialog Icon/carl_dialog.png")
@@ -63,6 +67,8 @@ func add_left_dialog_box():
 		short_node_text.text = "Dann geben wir jetzt noch einmal so richtig Gras!"
 		short_node_rect.texture = PRESET1
 		short_node_name.text = "Carl"
+		$"../VoiceOver".stream = load(carl_path + "4_02.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	elif dialogs == 4:
@@ -84,6 +90,8 @@ func add_left_dialog_box():
 		short_node_text.text = "Was müssen wir denn als nächstes machen?"
 		short_node_rect.texture = PRESET2
 		short_node_name.text = "Cari"
+		$"../VoiceOver".stream = load(cari_path + "4_05.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 		same_speaker = false
 	
@@ -92,9 +100,11 @@ func add_left_dialog_box():
 		var short_node_text = short_node.get_child(0)
 		var short_node_rect = short_node.get_child(1)
 		var short_node_name = short_node.get_child(3).get_child(0)
-		short_node_text.text = "Den richtigen Boden? Ich Pflanzen wachsen einfach so in der Erde?"
+		short_node_text.text = "Den richtigen Boden? Ich dachte Pflanzen wachsen einfach so in der Erde?"
 		short_node_rect.texture = PRESET1
 		short_node_name.text = "Carl"
+		$"../VoiceOver".stream = load(carl_path + "4_08.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	elif dialogs == 13:
@@ -102,9 +112,11 @@ func add_left_dialog_box():
 		var short_node_text = short_node.get_child(0)
 		var short_node_rect = short_node.get_child(1)
 		var short_node_name = short_node.get_child(3).get_child(0)
-		short_node_text.text = "Als König von Floratum musst du doch eigentlich wissen, welcher Boden der richtige ist, warum sagst du es uns nicht einfach?"
+		short_node_text.text = "Als König von Floratium musst du doch eigentlich wissen, welcher Boden der richtige ist, warum sagst du es uns nicht einfach?"
 		short_node_rect.texture = PRESET2
 		short_node_name.text = "Cari"
+		$"../VoiceOver".stream = load(cari_path + "4_11.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	
@@ -134,6 +146,8 @@ func add_right_dialog_box():
 		short_node_text.text = "Vorzügliche Arbeit ihr zwei. Unsere Reise durch das Pflanzenwunderland ist fast vorbei."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_01.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	elif dialogs == 3:
@@ -144,6 +158,8 @@ func add_right_dialog_box():
 		short_node_text.text = "HOHOHO."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_03.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	elif dialogs == 6:
@@ -154,6 +170,8 @@ func add_right_dialog_box():
 		short_node_text.text = "Die Vorbereitung für die Pflanzen ist nun vollzogen, jedoch fehlt noch ein wichtiger Schritt bevor wir den Garten für die Festivitäten planen können."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_06.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 		same_speaker = true
 	
@@ -165,6 +183,8 @@ func add_right_dialog_box():
 		short_node_text.text = "Wir müssen den richtigen Boden finden!"
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_07.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 		same_speaker = false
 	
@@ -173,9 +193,11 @@ func add_right_dialog_box():
 		var short_node_text = short_node.get_child(0)
 		var short_node_rect = short_node.get_child(1)
 		var short_node_name = short_node.get_child(3).get_child(0)
-		short_node_text.text = "Das stimmt so nicht ganz. Es gibt zwar Pflanzen, die fast überall wachsen können wie z.B. Löwenzahn."
+		short_node_text.text = "Das stimmt so nicht ganz. Es gibt zwar Pflanzen, die fast überall wachsen können wie zum Beispiel Löwenzahn."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_09_var1.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 		same_speaker = true
 	
@@ -187,6 +209,8 @@ func add_right_dialog_box():
 		short_node_text.text = "Aber viele Pflanzen benötigen einen besonderen Boden um gedeihen zu können."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_09_var2.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	elif dialogs == 11:
@@ -194,9 +218,11 @@ func add_right_dialog_box():
 		var short_node_text = short_node.get_child(0)
 		var short_node_rect = short_node.get_child(1)
 		var short_node_name = short_node.get_child(3).get_child(0)
-		short_node_text.text = "Dort drüben findet ihr mehrere Säcke gefüllt mit verschiedenen Bodentypen."
+		short_node_text.text = "Dort drüben findet ihr mehrere Säcke, gefüllt mit verschiedenen Bodentypen."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_10_var1.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	elif dialogs == 12:
@@ -204,9 +230,11 @@ func add_right_dialog_box():
 		var short_node_text = short_node.get_child(0)
 		var short_node_rect = short_node.get_child(1)
 		var short_node_name = short_node.get_child(3).get_child(0)
-		short_node_text.text = "Ich erklär euch wofür die genutzt werden und ihr sucht dann den richtigen aus."
+		short_node_text.text = "Ich erklär euch, wofür die genutzt werden und ihr sucht dann den richtigen aus."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_10_var2.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 		same_speaker = false
 	
@@ -215,9 +243,11 @@ func add_right_dialog_box():
 		var short_node_text = short_node.get_child(0)
 		var short_node_rect = short_node.get_child(1)
 		var short_node_name = short_node.get_child(3).get_child(0)
-		short_node_text.text = "Das könnte ich in der tat tun, aber wo bleibt dann der Spaß? Husch, Husch an die Arbeit mit auch."
+		short_node_text.text = "Das könnte ich in der Tat tun, aber wo bleibt dann der Spaß? Husch, husch, an die Arbeit mit auch."
 		short_node_rect.texture = PRESET4
 		short_node_name.text = "Guido"
+		$"../VoiceOver".stream = load(guido_path + "4_12.mp3")
+		$"../VoiceOver".play()
 		dialogs += 1
 	
 	
