fa3315d0ea7eed08d956baf190fad6d395397dad
Sw2ft <lars-kling@gmx.net>
2024-07-11 14:44:12 +0200

    schurbnel

--- Scenes and Scripts/Dialog/Garden Dialog/Minigame 2/garden_minigame_2_dialog_4.gd
+++ Scenes and Scripts/Dialog/Garden Dialog/Minigame 2/garden_minigame_2_dialog_4.gd
@@ -40,6 +40,7 @@ func _process(delta):
 	if dialogs == 4:
 		$"../VoiceOver".stop()
 		Global.dialog_playing = false
+		Global.pause_opened = false
 		get_tree().get_first_node_in_group("Animator").play("fade_out")
 		queue_free()
 ##########################################################################################
